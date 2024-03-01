4d784dbc5b1c14ad214ef9116bfd0cc4214af76b
bilberryy <bilberryyyyyyy@gmail.com>
2024-03-01 17:20:59 +0900

    dev-server setting

--- 1.gugudan/webpack.config.js
+++ 1.gugudan/webpack.config.js
@@ -1,8 +1,11 @@
 const path = require('path') // node깔면 딸려오는 애
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
+
 module.exports = {
   name: 'GuGuDan',
   mode: 'development',
-  devtool: 'eval',
+  devtool: 'inline-source-map', // hidden-source-map
   resolve: {
     extensions: ['.js', '.jsx'],
   },
@@ -10,19 +13,50 @@ module.exports = {
   entry: {
     app: './client',
   },
+
   module: {
     rules: [
       {
         test: /\.jsx?$/,
         loader: 'babel-loader',
         options: {
-          presets: ['@babel/preset-env', '@babel/preset-react'],
+          // presets: ['@babel/preset-env', '@babel/preset-react'],
+          presets: [
+            [
+              '@babel/preset-env',
+              {
+                targets: {
+                  browsers: ['> 1% in KR'], // browserslist
+                },
+                debug: true,
+              },
+            ],
+            '@babel/preset-react',
+          ],
+          plugins: ['react-refresh/babel'],
         },
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({ template: './index.html' }),
+    new ReactRefreshWebpackPlugin(),
+  ],
+
   output: {
     path: path.join(__dirname, 'dist'),
+    publicPath: '/dist/',
     filename: 'app.js',
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    host: 'localhost',
+    hot: true,
+    port: 3000,
+    open: true,
+    historyApiFallback: true, // 404 페이지 대신 index.html로 이동합니다.
+    // hot: true, // 모듈 전체를 다시 로드하지 않고 변경된 사항만 갱신합니다.
   },
 }
--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# react-webgame
-# react-webgame
+# 청양고추 vibe
+
+# 변경사항이 생길 때마다 빌드하는 거 귀찮아요 이럴 땐 webpack-dev-serve
+
+# npm install --save-dev webpack-dev-server
+
+# Webpack-dev-server v4부터 HMR이 기본적으로 활성화되어 있습니다
+
+# 그냥 dev server만 깔아도 새로고침 되는데?
+
+# ㄴㄴ, 전체가 새로고침 되기 때문에 모든 데이터 날아감
+
+# refresh깔면 바뀐 부분만 변경됨
+
+# npm install -D @pmmmwh/react-refresh-webpack-plugin react-refresh
+
+# peerOptional webpack-dev-server@"3.x || 4.x" from @pmmmwh/react-refresh-webpack-plugin@0.5.11
+
+# npm i -D webpack-dev-server@4.15.1
+
+# 겁 먹지 말고 에러 메세지를 차근차근 읽어봐요~~~~
+
+# html-webpack-plugin, script 태그를 사용하여 body에 모든 webpack 번들을 포함하는 HTML5 파일을 output.path에 자동으로 생성하게 해준다
+
+# plugins: new HtmlWebpackPlugin({ template: './index.html' })
+
+# 이렇게 설정하면 ./index.html파일을 참고해서 새로운html파일을 생성
